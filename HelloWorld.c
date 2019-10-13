4d98ec080b943a6b08e5befd0300e65a7732b9f5
lavanya28091999 <lavanyavcet1999@gmail.com>
2019-10-13 11:23:32 +0530

    new hello

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,6 +1,6 @@
-#include<stdio.h>
-void main()
-{
-   char a="hello world";
-   printf("%a",a);
-}
+    #include <stdio.h>
+    int main()
+    {
+       printf("Hello, World!");
+       return 0;
+    }
